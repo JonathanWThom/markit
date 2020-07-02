ed0a43c419fc5cc386f056bf3ca8b4ec794dbe3b
Jonathan Thom <jonathanthom@hey.com>
2020-07-01 19:02:27 -0700

    Add csv export to AverageSentiment

--- app/models/average_sentiment.rb
+++ app/models/average_sentiment.rb
@@ -3,4 +3,13 @@ class AverageSentiment < ApplicationRecord
   enum trait: { positive: 0, negative: 1, neutral: 2, mixed: 3}
 
   validates_uniqueness_of :value, scope: [:source, :trait, :date]
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << self.attribute_names
+      self.find_each do |record|
+        csv << record.attributes.values
+      end
+    end
+  end
 end
